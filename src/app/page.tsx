840dc55c815a0a299b71a8d17065d7bc5402f38e
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2025-05-10 21:47:16 -0300

    fix: img charset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
             fill
             style={{ objectFit: "contain" }}
             priority
-          ></Image>
+          />
         </motion.div>
       </div>
       <BgParticles />
